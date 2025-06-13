f05beac07e08c4a727bad410d0a322b32bdfe82a
Dip Ghosh <dipnsucse@gmail.com>
2025-06-13 22:58:33 +0600

    make database config env

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host'     => '192.168.56.56',
+        'port'     => 3306,
+        'dbname'   => 'PHP-For-Beginners',
+        'charset'  => 'utf8mb4',
+        'username' => 'homestead',
+        'password' => 'secret'
+    ]
+];
--- index.php
+++ index.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 require("functions.php");
 require("Database.php");
+
 //require("router.php");
 
 
-// connect to mysql database, and execute database a query.
-
-
-$db    = new Database();
-$posts = $db->query("Select * from posts")->fetchAll(PDO::FETCH_ASSOC);;
+$config = require("config.php");
+$db     = new Database($config['database'], $config['database']['username'], $config['database']['password']);
+$posts  = $db->query("Select * from posts")->fetchAll(PDO::FETCH_ASSOC);;
 
 
 foreach ($posts as $post) {

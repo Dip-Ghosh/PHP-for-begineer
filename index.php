9b78063847b086cd6bb3eb1a2dc83a5460083a83
Dip Ghosh <dipnsucse@gmail.com>
2025-06-13 22:13:18 +0600

    learning PDO first steps

--- index.php
+++ index.php
@@ -3,4 +3,18 @@
 declare(strict_types=1);
 
 require("functions.php");
-require("router.php");
+//require("router.php");
+
+
+// connect to mysql database.
+
+$dsn       = "mysql:host=192.168.56.56; port=3306;dbname=PHP-For-Beginners;charset=utf8mb4";
+$pdo       = new PDO($dsn, 'homestead', 'secret');
+$statement = $pdo->prepare("Select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($posts as $post) {
+    echo $post["title"] . "<br>";
+}
